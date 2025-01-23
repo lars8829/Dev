6390548eee96d280152a50ecd55290c15b37dda3
Jose Luis Lopez A <lars8829@gmail.com>
2025-01-23 15:37:20 -0500

    modificacion de diseño pulso btn

--- views/tecnicos/menu.php
+++ views/tecnicos/menu.php
@@ -7,45 +7,45 @@
                 <div class="nav">
                     <div class="sb-sidenav-menu-heading">Gestion Novedades</div>
 
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseNovedades" aria-expanded="false" aria-controls="collapseNovedades">
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseNovedades" aria-expanded="false" aria-controls="collapseNovedades" style="color:#0eb04f;">
                         <div class="sb-nav-link-icon"><i class="bi bi-tencent-qq"></i></div>Soporte
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
 
                     <div class="collapse" id="collapseNovedades" aria-labelledby="headingNovedades" data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="inicio.php">Panel Novedades</a>
-                            <a class="nav-link" href="">Panel Reportes</a>
-                            <a class="nav-link" href="solicitudes.php">Crear Novedad</a>
+                            <a class="nav-link" href="inicio.php" style="color:#081012;">Panel Novedades</a>
+                            <a class="nav-link" href="" style="color:#081012;">Panel Reportes</a>
+                            <a class="nav-link" href="solicitudes.php" style="color:#081012;">Crear Novedad</a>
                         </nav>
                     </div>
 
                     <div class="sb-sidenav-menu-heading">Gestion Inventarios</div>
 
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseInventarios" aria-expanded="false" aria-controls="collapseInventarios">
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseInventarios" aria-expanded="false" aria-controls="collapseInventarios" style="color:#00d4ff;">
                         <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>Equipos
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
 
                     <div class="collapse" id="collapseInventarios" aria-labelledby="headingInventarios" data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="contenido-software.php">Software</a>
-                            <a class="nav-link" href="contenido-hardware.php">Stock Partes</a>
-                            <a class="nav-link" href="contenido-inventarios.php">Inventarios</a>
-                            <a class="nav-link" href="contenido-movimientos.php">Movimientos</a>
+                            <a class="nav-link" href="contenido-software.php" style="color:#081012;">Software</a>
+                            <a class="nav-link" href="contenido-hardware.php" style="color:#081012;">Stock Partes</a>
+                            <a class="nav-link" href="contenido-inventarios.php" style="color:#081012;">Inventarios</a>
+                            <a class="nav-link" href="contenido-movimientos.php" style="color:#081012;">Movimientos</a>
                         </nav>
                     </div>
 
                     <div class="sb-sidenav-menu-heading">Gestion Mantenimientos</div>
 
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseMantenimientos" aria-expanded="false" aria-controls="collapseInventarios">
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseMantenimientos" aria-expanded="false" aria-controls="collapseInventarios" style="color:#e84e1b;">
                         <div class="sb-nav-link-icon"><i class="bi bi-clipboard2-plus-fill"></i></div>Seguimientos
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
 
                     <div class="collapse" id="collapseMantenimientos" aria-labelledby="headingMantenimientos" data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="contenido-mantenimientos.php">Mantenimientos</a>
+                            <a class="nav-link" href="contenido-mantenimientos.php" style="color:#081012;">Mantenimientos</a>
                         </nav>
                     </div>
                 </div>
--- views/tecnicos/nav.php
+++ views/tecnicos/nav.php
@@ -1,9 +1,39 @@
 <!-- Inicio  Navbar-->
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="inicio.php">Allee </a>
+            <a class="navbar-brand ps-3" href="inicio.php" style="color:#aaedfa;">Allee </a>
             <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href=""><i class="bi bi-activity"></i></button>
+            
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="">
+  <i class="bi bi-activity" style="color:rgb(241, 243, 242);"></i>
+</button>
+
+<style>
+  /* Efecto de pulso */
+  #sidebarToggle i {
+    display: block;
+    animation: pulseAnimation 1.5s infinite ease-in-out;
+  }
+
+  /* Animación de pulso: expansión y contracción */
+  @keyframes pulseAnimation {
+    0% {
+      transform: scale(1); /* Tamaño original */
+      color: rgb(241, 243, 242); /* Color inicial */
+    }
+    50% {
+      transform: scale(1.1); /* Expande ligeramente el icono */
+      color: #0eb04f; /* Color del pulso */
+    }
+    100% {
+      transform: scale(1); /* Vuelve al tamaño original */
+      color: rgb(241, 243, 242); /* Regresa al color original */
+    }
+  }
+</style>
+
+
+
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 
@@ -11,7 +41,7 @@
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i> <?php echo $usuario; ?></a>
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw" style="color:#aaedfa;"></i> <?php echo $usuario; ?></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="../../php/logout.php">Logout</a></li>
                     </ul>
